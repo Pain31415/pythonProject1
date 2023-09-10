aad1933e928e64023bb12b173b9de7252e6fc9d2
Pain31415 <Cherniy412@ukr.net>
2023-09-10 10:57:09 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+
+a=float(input('Enter a:'))
+b=float(input('Enter b '))
+print(f'Sum = {a+b}')
+print(f'Sub = {a-b}')
+print(f'Mul = {a*b}')
+print(f'Div = {a/b}')
+
+
+a=float(input('Enter a:'))
+b=float(input('Enter b '))
+print(f'{a%b}')
+
+a=int(input('Довжина'))
+b=int(input('Ширина'))
+c=(a+b)*2
+print(c)
